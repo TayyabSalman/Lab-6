37373ab2481489da5a75ba7aefd437c90cea34dd
TayyabSalman <31974963+TayyabSalman@users.noreply.github.com>
2017-10-20 23:26:14 +0500

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        //Create ConnectionString and Inser Statement
+        string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        string insertSql = "INSERT INTO Users (FirstName,LastName,UserName,Password,Email,Address,Gender) values (@FirstName,@LastName,@UserName,@Password,@Email,@Address,@Gender)";
+        //Create SQL connection
+        SqlConnection con = new SqlConnection(connectionString);
+        
+        //Create SQL Command And Sql Parameters
+
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = insertSql;
+
+        SqlParameter firstName = new SqlParameter("@FirstName", SqlDbType.VarChar, 50);
+        firstName.Value = txtFirstName.Text.ToString();
+        cmd.Parameters.Add(firstName);
+
+        SqlParameter lastName = new SqlParameter("@LastName", SqlDbType.VarChar, 50);
+        lastName.Value = txtLastName.Text.ToString();
+        cmd.Parameters.Add(lastName);
+
+        SqlParameter userName = new SqlParameter("@UserName", SqlDbType.VarChar, 50);
+        userName.Value = txtUserName.Text.ToString();
+        cmd.Parameters.Add(userName);
+
+        SqlParameter pwd = new SqlParameter("@Password", SqlDbType.VarChar, 50);
+        pwd.Value = txtPwd.Text.ToString();
+        cmd.Parameters.Add(pwd);
+
+        SqlParameter email = new SqlParameter("@Email", SqlDbType.VarChar, 50);
+        email.Value = txtEmailID.Text.ToString();
+        cmd.Parameters.Add(email);
+
+        SqlParameter address = new SqlParameter("@Address", SqlDbType.VarChar, 50);
+        address.Value = txtAdress.Text.ToString();
+        cmd.Parameters.Add(address);
+
+        SqlParameter gender = new SqlParameter("@Gender", SqlDbType.VarChar, 10);
+        gender.Value = rdoGender.SelectedItem.ToString();
+        cmd.Parameters.Add(gender);
+
+        
+        try
+        {
+            con.Open();
+            cmd.ExecuteNonQuery();
+            lblMsg.Text = "User Registration successful";
+            ClearControls(Page);
+        }
+        catch (SqlException ex)
+        {
+            string errorMessage = "Error in registring user";
+            errorMessage += ex.Message;
+            throw new Exception(errorMessage);
+
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    public static void ClearControls(Control Parent)
+    {
+        foreach (Control c in Parent.Controls)
+            ClearControls(c);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,1 @@
+My name is Khawaja Tayyab Salman
